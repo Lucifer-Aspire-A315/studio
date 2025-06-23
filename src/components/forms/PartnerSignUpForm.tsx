8b1fddedb7328698d8214b8857ec87f44a102b16
rn fintech <fintechrn@gmail.com>
2025-06-23 08:36:46 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/forms/PartnerSignUpForm.tsx
+++ src/components/forms/PartnerSignUpForm.tsx
@@ -326,7 +326,7 @@ export function PartnerSignUpForm() {
                                 <FormLabel className="font-normal">Company</FormLabel>
                               </FormItem>
                             </RadioGroup>
-                          </FormControl>
+                           </FormControl>
                           <FormMessage />
                         </FormItem>
                       )}
